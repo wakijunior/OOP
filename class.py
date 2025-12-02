25df164b89da59dc306534ea89f017346d7f75c0
Henry <hewstechcomps@gmail.com>
2025-12-02 18:48:17 +0300

    message

--- class.py
+++ class.py
@@ -1,15 +1,16 @@
 class BankAccount:
     def __init__(self, Account_number, balance, owners_name, date_opened):
         self.account_number = Account_number
-        self.balance = balance
+        self.balance = 0git
         self.owners_name = owners_name
         self.date_opened = date_opened
-
-        self.balance = 0
         print("Welcome")
     def deposit(self, amount):
         amount = float(input("Enter the Amount you want to deposit: "))
-        self.balance += amount
+        if amount < 0:
+            print("you can't deposit a negative amount")
+        else:
+            self.balance += amount
         print(f"You have deposited Ksh. {amount}")
         
     def withdraw(self, amount):
