df34841542f50ce3eee8e8c19d4ed5e7203058ed
Henry <hewstechcomps@gmail.com>
2025-12-09 21:03:17 +0300

    message

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from database import get_products, get_sales
+from flask import Flask, render_template, request, redirect, url_for
+from database import get_products, get_sales, insert_products
 
 app = Flask(__name__)
 
@@ -7,27 +7,44 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route('/products')
+def fetch_products():
+    products = get_products()
+    return render_template("products.html", products = products)
+
+
+
+@app.route('/addproducts', methods=['GET','POST'])
+def add_products():
+    product_name = request.form["product_name"]
+    buying_price = request.form["buying_price"]
+    selling_price = request.form["selling_price"]
+    new_product = (product_name,buying_price,selling_price)
+    insert_products(new_product)
+    return redirect(url_for('fetch_products'))
+   
+    
+
 @app.route('/sales')
 def sales():
     sales = get_sales()
-    return render_template("sales.html")
+    return render_template("sales.html",sales=sales)
+
 
 @app.route('/dashboard')
 def dashboard():
     return render_template("dashboard.html")
 
+
 @app.route('/login')
 def login():
     return render_template("login.html")
+
 
 @app.route('/register')
 def register():
     return render_template("register.html")
 
-@app.route('/products')
-def fetch_products():
-    products = get_products()
-    return render_template("products.html")
 
    
 
